77e66925fe175825929d1c6126b80f6944871227
djambred <djambred@gmail.com>
2021-11-03 13:50:22 +0700

    pertemuan 7

--- pert7/array/Array.java
+++ pert7/array/Array.java
@@ -2,7 +2,7 @@ class Array {
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5, 6};
         System.out.println("List Array:");
-        System.out.println("Array ke-0 : " + a[0]);
+        System.out.println("Array ke-0: " + a[0]);
         System.out.println("Array ke-1: " + a[1]);
         System.out.println("Array ke-2: " + a[2]);
         System.out.println("Array ke-3: " + a[3]);
