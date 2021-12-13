e97800c247ad3dd7514585e7f1b3a1c1ef54619b
djambred <djambred@gmail.com>
2021-12-14 00:28:26 +0700

    fresh from oven

---
+++ pert11/Inte.java
@@ -0,0 +1,11 @@
+public class Inte {
+    public static void inte (String[]args) {
+        int a = 20;
+        int b = 10;
+        System.out.println("Hasil dari a%b = " + (a%b));
+        System.out.println("Hasil dari a*b = " + (a*b));
+        System.out.println("Hasil dari a+b = " + (a+b));
+        System.out.println("Hasil dari a-b = " + (a-b));
+        System.out.println("Hasil dari a/b = " + (a/b)); 
+    }
+}
---
+++ pert11/Variabel.java
@@ -0,0 +1,11 @@
+public class Variabel {
+    public static void variabel (String[]args){
+        int x, y, c;
+        x=4&6;
+        y=5>>>2;
+        c=(2+4)*6;
+        System.out.println("Nilai y : " + y);
+        System.out.println("Nilai c : " + c);
+        System.out.println("Nilai x : " + x);
+    }
+}
