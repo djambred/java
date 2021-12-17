e577ce01469858e097348f7afd54f79ab81ddc18
djambred <djambred@gmail.com>
2021-12-17 20:59:33 +0700

    fresh from oven

--- connectiondatabase/Connect.java
+++ connectiondatabase/Connect.java
@@ -1,11 +1,16 @@
 import java.sql.*;
 
+
 class Connect {
     public static void main(String args[]){
+        String URL = "jdbc:mysql://192.168.42.48:23306/go_echo";
+        String user = "root";
+        String pass = "p455w0rd";
         try{
-            Connection con = DriverManager.getConnection("jdbc:mysql://172.25.187.200:23306/go_echo","root","p455w0rd");
+            Connection conn = DriverManager.getConnection(URL, user, pass);
             System.out.println("Sukses");
-            con.close();
+            conn.close();
+
         }catch(Exception e){
             System.out.println(e);
         }
