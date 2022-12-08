b2f64b0a3aa0da47ce445c33fadc7d215c62bada
djambred <djambred@gmail.com>
2022-12-08 11:51:06 +0700

    fresh from oven

--- tugasnow/Main.java
+++ tugasnow/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         
-        //Tugas tugas = new Tugas();
         PersegiP persegipanjang = new PersegiP(4,4);
-        Segitiga segitiga = new Segitiga(6, 3);
-        
-        // memanggil method luas dan keliling
-        //tugas.luas();        
+        Segitiga segitiga = new Segitiga(6, 3);    
         System.out.println("Luas persegi panjang: " + persegipanjang.luas());
-        
         System.out.println("Luas segitiga: " + segitiga.luas());
         
     }
