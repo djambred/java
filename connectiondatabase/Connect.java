b3e6b199508c6031d57f0f5f1aac0ee356b5e9f9
djambred <djambred@gmail.com>
2023-01-05 11:57:41 +0700

    crud sample tambahan

--- connectiondatabase/Connect.java
+++ connectiondatabase/Connect.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 class Connect {
     public static void main(String args[]){
-        String URL = "jdbc:mysql://192.168.42.48:23306/go_echo";
+        String URL = "jdbc:mysql://172.29.157.166:23306/go_echo";
         String user = "root";
         String pass = "p455w0rd";
         try{
